1bb7d0cb99799bf2733f4a7ce2211e12849fd842
Vinicius Teodoro <vinicius.teodoro@enacom.com.br>
2019-09-15 18:34:27 -0300

    Merging notebook in the server to return desired answer from request.

--- flask_mongo/pymongoexample/main.py
+++ flask_mongo/pymongoexample/main.py
@@ -1,62 +1,56 @@
-from flask import Blueprint
+from flask import Blueprint, request
+import requests
 import sys
+import json
 from .extensions import mongo 
-from .utils import vocab_return, ngram_vocab_return, vocab_vector_return, ngram_vector_return
+from .utils import vocab_return, ngram_vocab_return, vocab_vector_return, ngram_vector_return, build_dataframe
 
 main = Blueprint('main', __name__)
-
 import findspark
 findspark.init()
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('cin').getOrCreate()
-from pyspark.sql.functions import regexp_replace, lower, monotonically_increasing_id, lit
-from pyspark.ml.feature import Tokenizer, RegexTokenizer
-from pyspark.sql.functions import col, udf
-from pyspark.sql.types import IntegerType
-from pyspark.ml.feature import StopWordsRemover
-from pyspark.ml.feature import CountVectorizer
+# from requests.exceptions import HTTPError
+# from pyspark.sql.types import StructType, StructField, StringType
 
-
-@main.route('/postFiles')
+@main.route('/postFiles', methods=['POST'])
 def index():
     files_collection = mongo.db.files
-    files_collection.insert({'link' : 'somelink2'})
-    return '<h1>Added a User!</h1>'
+    data = request.json
+    response = requests.get(data['link']).text
+    filename = data['link'].rsplit("/", 1)
+    files_collection.insert({'name': filename[len(filename)-1], 'text': response})
+    return 'Added File'
+
+@main.route('/postText', methods=['POST'])
+def indexText():
+    files_collection = mongo.db.files
+    data = request.json
+    files_collection.insert({'name': data['name'], 'text': data['text']})
+    return 'Added Text'
+
 
 @main.route('/getVocabulary')
 def vocab():
-    files_collection = mongo.db.files
-    files = files_collection.find({})
-    print(files[0]['link'], file=sys.stderr)
-    vocab_data = vocab_return(spark, files[0]['link'])
-    print(vocab_data, file=sys.stderr)
+    df = build_dataframe(spark)
+    vocab_data = vocab_return(df)
     return vocab_data
 
 @main.route('/getNgramVocabulary')
 def ngram_vocab():
-    files_collection = mongo.db.files
-    files = files_collection.find({})
-    print(files[0], file=sys.stderr)
-    ngram_data = ngram_vocab_return(spark, files[0]['link'])
+    df = build_dataframe(spark)
+    ngram_data = ngram_vocab_return(df)
     print(ngram_data, file=sys.stderr)
     return ngram_data
 
 @main.route('/getVector')
 def vector():
-    files_collection = mongo.db.files
-    files = files_collection.find({})
-    print(files[0], file=sys.stderr)
-    links = ['data.txt', 'class.txt', 'newtest.txt']
-    vector_data = vocab_vector_return(spark, links)
-    print(vector_data, file=sys.stderr)
+    df = build_dataframe(spark)
+    vector_data = vocab_vector_return(df)
     return vector_data
 
 @main.route('/getNgramVector')
 def ngram_vec():
-    files_collection = mongo.db.files
-    files = files_collection.find({})
-    print(files[0], file=sys.stderr)
-    links = ['data.txt', 'class.txt', 'newtest.txt']
-    ngram_vector_data = ngram_vector_return(spark, links)
-    print(ngram_vector_data, file=sys.stderr)
+    df = build_dataframe(spark)
+    ngram_vector_data = ngram_vector_return(df)
     return ngram_vector_data
--- flask_mongo/pymongoexample/utils.py
+++ flask_mongo/pymongoexample/utils.py
@@ -3,17 +3,27 @@ from pyspark.ml.feature import Tokenizer, RegexTokenizer
 from pyspark.ml.feature import StopWordsRemover
 from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.feature import NGram
+import sys
+from .extensions import mongo
 
-def vocab_return(spark, links):
-    pre_data = pre_process_data(spark, links)
+
+def build_dataframe(spark):
+    files_collection = mongo.db.files
+    files = list(files_collection.find())
+    dataframe = [tuple([files[i]['name'], files[i]['text']]) for i in range(0,len(files))]
+    df = spark.createDataFrame(dataframe, ['name', 'text'])
+    return df
+
+def vocab_return(df):
+    pre_data = pre_process_data(df)
     count_model = count_vectorizer(pre_data)
     dict_vocab = {
         "vocabulary": count_model.vocabulary
     }
     return dict_vocab
 
-def ngram_vocab_return(spark, links):
-    pre_data = pre_process_data(spark, links)
+def ngram_vocab_return(df):
+    pre_data = pre_process_data(df)
     ngram_data = ngram(pre_data)
     count_model = count_vectorizer(ngram_data,"ngrams")
     dict_ngram = {
@@ -21,23 +31,22 @@ def ngram_vocab_return(spark, links):
     }
     return dict_ngram
 
-def vocab_vector_return(spark, links):
-    pre_data = pre_process_data(spark, links)
+def vocab_vector_return(df):
+    pre_data = pre_process_data(df)
     count_model = count_vectorizer(pre_data)
-    return build_vector(count_model, pre_data, links)
+    return build_vector(count_model, pre_data)
 
-def ngram_vector_return(spark, links):
-    pre_data = pre_process_data(spark, links)
+def ngram_vector_return(df):
+    pre_data = pre_process_data(df)
     ngram_data = ngram(pre_data)
     count_model = count_vectorizer(ngram_data,"ngrams")
-    return build_vector(count_model, ngram_data, links)
+    return build_vector(count_model, ngram_data)
 
-def pre_process_data(spark, links):
-    df = spark.read.option("encoding", "ISO-8859-1").text(['../data.txt', '../class.txt', '../newtest.txt'])
-    df_collumn = df.withColumn("value", regexp_replace(lower(df["value"]), "[$&+,:;=?@#|'<>.-^*()%!]", ""))
+def pre_process_data(df):
+    df_collumn = df.withColumn("text", regexp_replace(lower(df["text"]), "[$&+,:;=?@#|'<>.-^*()%!]", ""))
     df_read = df_collumn.select('*').withColumn("id", monotonically_increasing_id())
     # Tokenize data
-    tokenizer = Tokenizer(inputCol="value", outputCol="words")
+    tokenizer = Tokenizer(inputCol="text", outputCol="words")
     df_tokenized = tokenizer.transform(df_read)
     #Remove Stop Words
     language = "portuguese"
@@ -57,10 +66,13 @@ def ngram(df_clean):
     ngramDataFrame = ngram.transform(df_clean)
     return ngramDataFrame
 
-def build_vector(model, pre_data, links):
+def build_vector(model, pre_data):
+    print(pre_data.select("ngrams").show(truncate=False), file=sys.stderr)
     result = model.transform(pre_data)
+    name_list = result.select('name').collect()
+    names = [row.name for row in name_list]
     selected = result.select('features')
     dict_vector = {}
-    for ind, x in enumerate(links):
-        dict_vector[links[ind]] = list(map(int, selected.collect()[ind][0].toArray()))
+    for ind, x in enumerate(names):
+        dict_vector[x] = list(map(int, selected.collect()[ind][0].toArray()))
     return dict_vector
